eb0d8c69a06f1fb46455c4bfcfb26ef0a1c8c520
Benjamin Ehrlich <bensaysstuff@gmail.com>
2020-10-28 12:09:58 -0400

    Uncoupling game.

--- game/sprites/general.js
+++
@@ -1,2 +0,0 @@
-spr_door = new Sprite("spr_door","game/sprites/tiles.png",64,224, 32,48,32,48);
-spr_door_open = new Sprite("spr_door_open","game/sprites/tiles.png",32,224, 32,48,32,48);
--- game/tiles/general.js
+++
@@ -1,6 +0,0 @@
-let spr_floor = new Sprite("spr_floor","game/sprites/tilese2.png",99,0,32,48,32,48,.1);
-let spr_floor_shadow = new Sprite("spr_floor_shadow","game/sprites/tilese2.png",165,0,32,48,32,48,.1);
-let spr_wall = new Sprite("spr_wall","game/sprites/tilese2.png",0,0,32,48,32,48,.1);
-let spr_ceiling = new Sprite("spr_ceiling","game/sprites/tilese2.png",132,0,32,48,32,48,.1); 
-let spr_ceiling_cave = new Sprite("spr_ceiling","game/sprites/tiles.png",288,32,32,48,32,48,.1); 
-let spr_water = new Sprite("spr_water","game/sprites/tiles.png",[0,32],176,32,48,32,48);
